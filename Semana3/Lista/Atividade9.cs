f6cbc7eece8de70333f0075b6f566ee5b14f79d5
l-m-duarte <160736732+l-m-duarte@users.noreply.github.com>
2026-08-26 21:11:55 -0300

    Update Atividade9.cs

--- Semana3/Lista/Atividade9.cs
+++ Semana3/Lista/Atividade9.cs
@@ -1,1 +1,42 @@
+using System;
+
+class Atividade9
+{
+    static string gerarComplementar(string dna)
+    {
+        string complement = "";
+
+        for (int i=0; i < dna.Length; i++)
+        {
+            if (dna[i] == 'A')
+            {
+                complement += "T";
+            }
+            else if (dna[i] == 'T')
+            {
+                complement += "A";
+            }
+            else if (dna[i] == 'C')
+            {
+                complement += "G";
+            }
+            else if (dna[i] == 'G')
+            {
+                complement += "C";
+            }
+        }
+        return complement;
+    }
+    
+    
+    
+    static void Main()
+    {
+        string dna = "";
+        Console.Write("Digite a fita de DNA: ");
+        dna = Console.ReadLine()!;
+
+        Console.WriteLine($"Fita complementar: {gerarComplementar(dna)}");
+    }
+}
 
