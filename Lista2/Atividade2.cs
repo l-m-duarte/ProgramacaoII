45e583f0a0a33188945a4c17334f1efc933d1c13
l-m-duarte <160736732+l-m-duarte@users.noreply.github.com>
2026-09-17 14:42:13 -0300

    Create Atividade2.cs

---
+++ Lista2/Atividade2.cs
@@ -0,0 +1,73 @@
+class Program
+{
+    static int[,] gerarMatriz(int[,] matriz)
+    {
+        Random random = new Random();
+        int linhas = matriz.GetLength(0);
+        int cols = matriz.GetLength(1);
+
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matriz[i, j] = random.Next(0, 100);
+            }
+        }
+        return matriz;
+    }
+    
+    static void lerMatriz(int[,] matriz)
+    {
+        int linhas = matriz.GetLength(0);
+        int cols = matriz.GetLength(1);
+        //lendo a matriz
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"[{i},{j}]:");
+                matriz[i, j] = int.Parse(Console.ReadLine());
+            }// fim for j
+        }// fim for i
+    }// fim funcao ler
+
+    // mostraMatriz
+    public static void mostrarMatriz(int[,] matriz)
+    {
+        int linhas = matriz.GetLength(0);
+        int cols = matriz.GetLength(1);
+        // mostrar a matriz
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"|{matriz[i, j],3}");
+            }// fim j
+            Console.WriteLine();// quebra a linha
+        }// fim i
+    }
+    
+    public static void Main(){
+        Console.WriteLine("linhas da matriz:");
+        int linhas = int.Parse(Console.ReadLine());
+        Console.WriteLine("colunas da matriz:");
+        int colunas = int.Parse(Console.ReadLine());
+
+        int[,] matriz = new int[linhas,colunas];
+        matriz = gerarMatriz(matriz);
+        
+        mostrarMatriz(matriz);
+        
+        int menor = matriz[0,0];
+        
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                if (matriz[i,j] < menor){ menor = matriz[i,j];}
+            }// fim j
+        }// fim i
+        
+        Console.WriteLine($"Menor valor da matriz: {menor}");
+    }
+}
